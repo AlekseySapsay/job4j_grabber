feb4e9330183fcfba982a7e39943657886cbc9e5
AlekseySapsay <pheniksesalex@mail.ru>
2021-10-19 21:29:08 +0300

    1. Quartz [#175122]добавил чтение файла

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,23 +3,37 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
+/**
+ * https://job4j.ru/profile/exercise/56/task-view/353
+ *
+ * Программа выполняет переодические действия. Выводит сообщения на консоль
+ * используя планировщик Quartz
+ * http://www.quartz-scheduler.org/.
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 19.10.2021
+ */
+
 public class AlertRabbit {
     public static void main(String[] args) {
-        // readProperties
-        int a = readProperties();
+        AlertRabbit alertRabbit = new AlertRabbit();
+        int periodInSeconds = alertRabbit.readProperties();
 
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(1)
+                    .withIntervalInSeconds(periodInSeconds)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -38,15 +52,17 @@ public class AlertRabbit {
         }
     }
 
-    public static int readProperties() {
-        InputStream stream = AlertRabbit.class.getResourceAsStream(
-                "C:\\projects\\job4j_grabber\\src\\main\\resources");
-        System.out.println(stream != null);
-        stream = AlertRabbit.class.getClassLoader().getResourceAsStream(
-                "C:\\projects\\job4j_grabber\\src\\main\\resources");
-        System.out.println(stream != null);
-        System.out.println(stream);
+    public int readProperties() {
+        List<String> stringList = new ArrayList<>();
 
-        return 0;
+        try (BufferedReader in = new BufferedReader(new FileReader(
+                "C:\\projects\\job4j_grabber\\src\\main\\resources\\rabbit.properties"))) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                stringList = Arrays.asList(line.split("="));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Integer.parseInt(stringList.get(1));
     }
 }
