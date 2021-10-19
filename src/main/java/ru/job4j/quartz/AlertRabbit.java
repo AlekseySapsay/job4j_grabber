c6b495b99a5ca1a74c4ab4ba13604354a8b7f2b8
AlekseySapsay <pheniksesalex@mail.ru>
2021-10-19 03:44:56 +0300

    1. Quartz [#175122]добавил чтение файла

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,18 +2,24 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.InputStream;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
+        // readProperties
+        int a = readProperties();
+
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(1)
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -30,5 +36,17 @@ public class AlertRabbit {
         public void execute(JobExecutionContext context) throws JobExecutionException {
             System.out.println("Rabbit runs here ...");
         }
+    }
+
+    public static int readProperties() {
+        InputStream stream = AlertRabbit.class.getResourceAsStream(
+                "C:\\projects\\job4j_grabber\\src\\main\\resources");
+        System.out.println(stream != null);
+        stream = AlertRabbit.class.getClassLoader().getResourceAsStream(
+                "C:\\projects\\job4j_grabber\\src\\main\\resources");
+        System.out.println(stream != null);
+        System.out.println(stream);
+
+        return 0;
     }
 }
