c8243d5cb1b3b82b3a133fce7fb0576ef540d2cb
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-21 04:26:17 +0300

    4. Схема таблицы Post [#1731] изменил объем

--- db/scripts/schemaPost.sql
+++ db/scripts/schemaPost.sql
@@ -1,6 +1,6 @@
 create table if not exists post(
     id serial primary key,
-    title varchar(1000),
+    title varchar(256),
     description text,
     link varchar(1000) unique,
     created timestamp
