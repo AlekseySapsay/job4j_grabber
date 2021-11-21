f7f9610458bebdec35983aa2db93b94184aa8f02
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-21 04:25:27 +0300

    4. Схема таблицы Post [#1731] изменил название таблицы rabbit на schemaRabbit.sql и добавил schemaPost.sql

--- db/scripts/schemaPost.sql
+++ db/scripts/schemaPost.sql
@@ -1,6 +1,6 @@
 create table if not exists post(
     id serial primary key,
-    title varchar(256),
+    title varchar(1000),
     description text,
     link varchar(1000) unique,
     created timestamp
