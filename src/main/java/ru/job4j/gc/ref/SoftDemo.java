b99f72ade48579533e6050ede49d290f509dd122
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-24 12:54:44 +0300

    1. Демонстрация работы GC [#1589 #230868] изучаю стипы ссылок

---
+++ src/main/java/ru/job4j/gc/ref/SoftDemo.java
@@ -0,0 +1,53 @@
+package ru.job4j.gc.ref;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://job4j.ru/profile/exercise/61/task-view/375
+ * <p>
+ * Изучение работы GC. Виды ссылок
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 22.11.2021
+ */
+public class SoftDemo {
+
+    public static void main(String[] args) {
+        example1();
+        //example2();
+    }
+
+    private static void example1() {
+        Object strong = new Object();
+        SoftReference<Object> soft = new SoftReference<>(strong);
+        strong = null;
+        System.out.println(soft.get());
+    }
+
+    private static void example2() {
+        List<SoftReference<Object>> objects = new ArrayList<>();
+        for (int i = 0; i < 10_000; i++) {
+            objects.add(new SoftReference<Object>(new Object() {
+                String value = String.valueOf(System.currentTimeMillis());
+
+                @Override
+                protected void finalize() throws Throwable {
+                    System.out.println("Object removed!");
+                }
+            }));
+        }
+        System.gc();
+        int liveObject = 0;
+        for (SoftReference<Object> ref : objects) {
+            Object object = ref.get();
+            if (object != null) {
+                liveObject++;
+            }
+        }
+        System.out.println(liveObject);
+    }
+
+}
